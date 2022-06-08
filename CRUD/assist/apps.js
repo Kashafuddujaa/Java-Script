7b823c682f9b1924c263c10ddb0b3055e5ad5b03
Kashaf Ud Duja <99113718+Kashafuddujaa@users.noreply.github.com>
2022-06-08 16:33:45 +0500

    Update apps.js
    Attendace Sheet

--- CRUD/assist/apps.js
+++ CRUD/assist/apps.js
@@ -1,25 +1,45 @@
-var rollno=[rollno]
+var rollno=[]
 var input=document.querySelector("#rollno");
+var p = document.querySelector("#totalAttendance");
+
 function addrecord (){
     rollno.push(input.value)
     input.value=""
-    ShowToPage=(rollno);
     console.log=(rollno);
+    showDataToPage();
 }
-var p=document.querySelector("#view")
+function showDataToPage(){
+    p.innerHTML="Total attendance is" + rollno.length
+    console.log(rollno)
+}
+
+function deleterecord (){
+    var index = rollno.indexOf(input.value)
+    if (index !== -1){
+        rollno.splice(index, 1)
+        input.value = ""
+    } else {
+        alert("data not found")
+    }
+            console.log(rollno)
+}
+ 
 function viewrecord (){
-    p.innerHTML="Total Record is " + rollno.length
+    for(i=0; i < rollno.length; i++)
+    document.write(rollno[i] + "<br/>")
 }
-var p=document.querySelector("#delete");
-function deleterecord(){
-    deleterecord.getElementById('type', 'button');    // SET INPUT ATTRIBUTE.
-    deleterecord.getElementById('value', 'Delete');
-    // this.td.appendChild(btDelete);
-}
-var p=document.querySelector("#update")
+
 function updaterecord (){
-    updaterecord.getElementById('value', 'Update');
-    updaterecord.getElementById('id', 'Edit' + i);
+    var updaterec = rollno.indexOf(input.value)
+    console.log(updaterec)
+    if (updaterec !== -1){
+        var update = prompt("Which value do you like to update?" , updaterec)
+        rollno.splice(updaterec, 1, update)
+    } else {
+        alert("Data doesn't exist in array")
+    }
+    console.log(rollno)
+
 }
 
 
